a43756397af6098477eae9ae2d6726dc8dcbc6a1
TheMusicalWeeb <82478075+TheMusicalWeeb@users.noreply.github.com>
2022-10-28 14:59:50 +1300

    TitleScreen
    Started work on title screen

---
+++ TitleScreen.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+func fadeout():
+	$AnimationPlayer.play("FadeOut")
+	yield($AnimationPlayer,"animation_finished")
+
+
+	
+func _on_START_pressed():
+	fadeout()
+	get_tree().change_scene("res://Level1.tscn")
+	
